8597b26e4d27626ebf53f3e86b97de7d5aefadf9
Jagraj Singh <singhjagraj900@gmail.com>
2020-04-13 23:47:04 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,10 @@
-for i in list(range(20)):
-    if i % 3 == 0 and i % 5 == 0:
+# for every integer in a set range n
+for i in list(range(100)):
+    if i % 3 == 0 and i % 5 == 0: # has to be first if statement otherwise FizzBuzz doesn't work
         print("FizzBuzz")
     elif i % 3 == 0:
         print("Fizz")
     elif i % 5 == 0:
         print("Buzz")
     else:
-        print(i)
+        print(i) # ensures loop continues
